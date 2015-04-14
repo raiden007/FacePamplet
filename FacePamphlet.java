6fac39f6317d3b4d056ad70013c8c1129d427a31
S <s>
2015-04-14 14:28:26 +0300

    {"type":"TI", "runs":"1"}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -82,7 +82,7 @@ public class FacePamphlet extends ConsoleProgram
 				if (database.containsProfile(username) == false) {
 					profile = new FacePamphletProfile(nameTxtField.getText());
 					database.addProfile(profile);
-					println (profile.getName() + "added");
+					println (profile.getName() + " was added!");
 				} else {
 					println ("Profile already exists: " + profile.toString());
 				}
